24fb2cc958696bdf092a6005c1aad7c70cfa40b7
sethu1012 <sethu10121994@gmail.com>
2025-03-08 17:33:04 +0530

    ft: :bug: fixed eslint bugs

--- src/pages/patientMessage/index.tsx
+++ src/pages/patientMessage/index.tsx
@@ -1,54 +1,62 @@
-import { useSearch } from '@tanstack/react-router';
-import { useState, useEffect } from 'react';
-import type { User, Channel as StreamChannel } from 'stream-chat';
-import { useCreateChatClient, Chat, Channel, ChannelHeader, MessageInput, MessageList, Thread, Window } from 'stream-chat-react';
+import { useState, useEffect } from "react";
+import type { User, Channel as StreamChannel } from "stream-chat";
+import {
+  useCreateChatClient,
+  Chat,
+  Channel,
+  ChannelHeader,
+  MessageInput,
+  MessageList,
+  Thread,
+  Window,
+} from "stream-chat-react";
 
-import 'stream-chat-react/dist/css/v2/index.css';
-import { messagePatientsRoute } from '../../App';
+import "stream-chat-react/dist/css/v2/index.css";
+import { messagePatientsRoute } from "../../App";
 
-const apiKey = 'te6nba3a55ru';
-const userId = 'thillai22';
+const apiKey = "te6nba3a55ru";
+const userId = "thillai22";
 
 const user: User = {
-    id: 'thillai22',
-    name: 'thillai',
+  id: "thillai22",
+  name: "thillai",
 };
 
 const App = () => {
-    const search = messagePatientsRoute.useSearch();
-    const [channel, setChannel] = useState<StreamChannel>();
-    const client = useCreateChatClient({
-        apiKey,
-        tokenOrProvider: search.token,
-        userData: user,
+  const search = messagePatientsRoute.useSearch();
+  const [channel, setChannel] = useState<StreamChannel>();
+  const client = useCreateChatClient({
+    apiKey,
+    tokenOrProvider: search.token,
+    userData: user,
+  });
+
+  useEffect(() => {
+    if (!client) return;
+
+    const channel = client.channel("messaging", "custom_channel_id", {
+      image: "https://getstream.io/random_png/?name=react",
+      name: "PVT healthcare services",
+      members: [userId],
     });
 
-    useEffect(() => {
-        if (!client) return;
+    setChannel(channel);
+  }, [client]);
 
-        const channel = client.channel('messaging', 'custom_channel_id', {
-            image: 'https://getstream.io/random_png/?name=react',
-            name: 'PVT healthcare services',
-            members: [userId],
-        });
+  if (!client) return <div>Setting up client & connection...</div>;
 
-        setChannel(channel);
-    }, [client]);
-
-    if (!client) return <div>Setting up client & connection...</div>;
-
-    return (
-        <Chat client={client}>
-            <Channel channel={channel}>
-                <Window>
-                    <ChannelHeader />
-                    <MessageList />
-                    <MessageInput />
-                </Window>
-                <Thread />
-            </Channel>
-        </Chat>
-    );
+  return (
+    <Chat client={client}>
+      <Channel channel={channel}>
+        <Window>
+          <ChannelHeader />
+          <MessageList />
+          <MessageInput />
+        </Window>
+        <Thread />
+      </Channel>
+    </Chat>
+  );
 };
 
 export default App;
