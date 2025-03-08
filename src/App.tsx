2f5d2c08244b358e82c84199c59634771d4e9c72
sethu1012 <sethu10121994@gmail.com>
2025-03-08 11:30:24 +0530

    ft: :sparkles: added base plugins

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,36 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {
+  Outlet,
+  RouterProvider,
+  createRootRoute,
+  createRoute,
+  createRouter,
+} from "@tanstack/react-router";
+import { TanStackRouterDevtools } from "@tanstack/router-devtools";
+import Home from "./pages/home";
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
+const rootRoute = createRootRoute({
+  component: () => (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Outlet />
+      <TanStackRouterDevtools />
     </>
-  )
-}
+  ),
+});
 
-export default App
+const homeRoute = createRoute({
+  getParentRoute: () => rootRoute,
+  path: "/",
+  component: Home,
+});
+
+// add other paths here
+
+const routeTree = rootRoute.addChildren([homeRoute]);
+
+const router = createRouter({
+  routeTree,
+});
+
+export default function App() {
+  return <RouterProvider router={router} />;
+}
